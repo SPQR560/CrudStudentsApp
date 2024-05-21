23241ccd8282d532c258c4176bcf9b21c42c58e7
Eugene <evgeny4815@gmail.com>
2024-05-21 23:26:14 +0500

    #3 init slim

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,3 +1,22 @@
 <?php
 
-echo 'Hello world';
+declare(strict_types=1);
+
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->addErrorMiddleware(true, false, false);
+
+// Add route callbacks
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write('Hello World, and good night');
+    return $response;
+});
+
+// Run application
+$app->run();
