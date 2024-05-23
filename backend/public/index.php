50537d5323bfe6f02dae64a054d904d797e60be7
Eugene <evgeny4815@gmail.com>
2024-05-24 00:01:03 +0500

    #3 wip add symfony/dotenv

--- backend/public/index.php
+++ backend/public/index.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 use Slim\Factory\AppFactory;
 use Spqr560\StudentsRoot\Layers\Infrastructure\Config\Route\Route;
+use Symfony\Component\Dotenv\Dotenv;
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->addErrorMiddleware(true, false, false);
+//(new Dotenv())->load(dirname(__DIR__) . '/.env');
+
+
+$app->addErrorMiddleware((bool)getenv('BACKEND_DEBUG_MODE'), false, false);
 
 // Add route callbacks
 Route::initRoutes($app);
