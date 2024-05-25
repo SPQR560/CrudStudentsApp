12a57a39f8ca371d21c8e1da1bfc7a410dfa2d0a
Eugene <evgeny4815@gmail.com>
2024-05-25 13:43:59 +0500

    #3 make closure at index

--- backend/public/index.php
+++ backend/public/index.php
@@ -9,7 +9,6 @@ use Symfony\Component\Dotenv\Dotenv;
 $projectPath = dirname(__DIR__);
 require_once $projectPath . '/vendor/autoload.php';
 
-$app = AppFactory::create();
 
 if (file_exists($projectPath . '/.env')) {
     (new Dotenv())->usePutenv()->load($projectPath . '/.env');
@@ -17,10 +16,14 @@ if (file_exists($projectPath . '/.env')) {
     throw new RuntimeException('dot env file is not found');
 }
 
-$app->addErrorMiddleware((bool)getenv('BACKEND_DEBUG_MODE'), false, false);
+$applicationClosure = function () {
+    $app = AppFactory::create();
+    $app->addErrorMiddleware((bool)getenv('BACKEND_DEBUG_MODE'), false, false);
 
-// Add route callbacks
-Route::initRoutes($app);
+    // Add route callbacks
+    Route::initRoutes($app);
 
-// Run application
-$app->run();
+    // Run application
+    $app->run();
+};
+$applicationClosure();
