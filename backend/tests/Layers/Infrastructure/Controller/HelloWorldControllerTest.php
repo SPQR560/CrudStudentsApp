d1572c5e107b158fe36a8c057062d73a44f82e02
Eugene <evgeny4815@gmail.com>
2024-05-25 18:26:50 +0500

    #4 add test and make file command

---
+++ backend/tests/Layers/Infrastructure/Controller/HelloWorldControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spqr560\Tests\Layers\Infrastructure\Controller;
+
+use Laminas\Diactoros\ServerRequest;
+use PHPUnit\Framework\TestCase;
+use Spqr560\StudentsRoot\Layers\Infrastructure\Controller\HelloWorldController;
+
+class HelloWorldControllerTest extends TestCase
+{
+    public function testHandle(): void
+    {
+        //arange
+        $action = new HelloWorldController();
+
+        //act
+        $response = $action->handle(new ServerRequest());
+
+        //assert
+        self::assertEquals(200, $response->getStatusCode());
+        $content = $response->getBody()->getContents();
+        self::assertEquals(
+            ['message' => "Hello World, good night and sleep well "],
+            json_decode($content, true)
+        );
+    }
+}
