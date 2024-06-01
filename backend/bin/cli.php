8f7e17ddf7787ddab718a616f06e3c596bbe8dd4
Eugene <evgeny4815@gmail.com>
2024-06-01 18:32:53 +0500

    #4 add symfony commands to cli and fix some bugs

--- backend/bin/cli.php
+++ backend/bin/cli.php
@@ -3,9 +3,12 @@
 
 declare(strict_types=1);
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
 use Spqr560\StudentsRoot\Layers\Infrastructure\Config\Container\ContainerConfig;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Dotenv\Dotenv;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
 $projectPath = dirname(__DIR__);
 require_once $projectPath.'/vendor/autoload.php';
@@ -19,6 +22,8 @@ if (file_exists($projectPath.'/.env')) {
 $container = (new ContainerConfig())->getContainer();
 $cli = new Application('Console application');
 
+$singleManagerProvider = new SingleManagerProvider($container->get(EntityManagerInterface::class));
+ConsoleRunner::addCommands($cli, $singleManagerProvider);
 
 $commands = $container->get('console-setting')['commands'];
 foreach ($commands as $command) {
--- backend/src/Layers/Infrastructure/Config/Container/ContainerConfig.php
+++ backend/src/Layers/Infrastructure/Config/Container/ContainerConfig.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Spqr560\StudentsRoot\Layers\Infrastructure\Config\Container;
 
 use DI\Container;
+use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Container\ContainerInterface;
 use Doctrine\ORM\ORMSetup;
 use Spqr560\StudentsRoot\Layers\Infrastructure\Config\Container\EnvType\EnvTypeEnum;
 use Spqr560\StudentsRoot\Layers\Infrastructure\Config\Container\EnvType\EnvTypeGetter;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 readonly class ContainerConfig
 {
@@ -36,19 +38,24 @@ readonly class ContainerConfig
                 'devMode' => $envType !== EnvTypeEnum::PROD,
                 'connection' => [
                     'url' => getenv('DB_URL'),
+                    'driver' => 'pdo_pgsql',
                 ],
-                'pathToXML' => 'Layers/Infrastructure/Config/Doctrine/xmlSchemaMap.xml',
+                'pathsToXML' => [
+                    'Layers/Infrastructure/Config/Doctrine/xmlSchemaMap.xml',
+                ],
+                'cacheDir' => 'var/cache/doctrine',
             ];
         });
 
         $this->container->set(EntityManagerInterface::class, function (ContainerInterface $container) {
-            $config = $container['doctrine-config'];
+            $config = $container->get('doctrine-config');
             $xmlSchemeConfiguration = ORMSetup::createXMLMetadataConfiguration(
-                $config['pathToXML'],
-                $config['connection'],
+                $config['pathsToXML'],
+                $config['devMode'],
+                cache: new FilesystemAdapter(directory: $config['cacheDir'])
             );
             return new EntityManager(
-                $config['connection'],
+                DriverManager::getConnection($config['connection']),
                 $xmlSchemeConfiguration
             );
         });
